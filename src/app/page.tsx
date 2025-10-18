1c56143149cfd05f43b7d25c4cb8ee8b476db65c
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2025-10-18 14:39:09 +0200

    feat: add Characters page and update Home page navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <h1>Welcome to the Home Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/characters">Go to Characters Page</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
 }
